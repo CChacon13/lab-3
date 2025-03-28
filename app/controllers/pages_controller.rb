0c998101964fa3dabcec515570fd5ae1807c4f42
CChacon13 <cichacon1@miuandes.com>
2025-03-28 16:03:44 -0300

    solucion problemas primer commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,15 +2,15 @@
 class PagesController < ApplicationController
     def home
     end
-  
+
     def about
     end
-  
+
     def contact
     end
-  
+
     def submit_contact
       flash[:notice] = "Thank you for your message, we will contact you soon."
       redirect_to contact_path, status: :see_other
     end
-  end
+end
